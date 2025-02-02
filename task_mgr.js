4817ada8f87b709e13bb1a7cf319d756204ee193
Kris <109919530+maleker86@users.noreply.github.com>
2025-02-02 11:52:14 -0800

    initialize project constructor

--- task_mgr.js
+++ task_mgr.js
@@ -1,7 +1,17 @@
 var task_list = [];
 
-function createTask() {
-    console.log("Test here");
-}
+// Constructor Function for Task objects
+function Task(proj,tasks,desc,due,priority) {
+    this.proj_title = proj;
+    this.tasks_list = tasks;
+    this.dueDate = due;
+    this.description = desc;
+    this.priority = priority;
+  }
 
-createTask();
+// Create a Person object
+const task1 = new Task("do dishes",["wash plate","wash fork"],"make sink clean",2/5/2025,1);
+
+// Display age
+console.log("My project is " + task1.proj_title + " and it involves " + task1.tasks_list);
+console.log(task1);
