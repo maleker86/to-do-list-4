e12a84f39eca7de4ac68e17501d5d5d3fe7db7d7
Kris <109919530+maleker86@users.noreply.github.com>
2025-02-02 13:46:52 -0800

    list each task in the arr

--- task_mgr.js
+++ task_mgr.js
@@ -1,17 +1,46 @@
 var task_list = [];
 
+let proj = document.getElementById("proj");
+let tasks = document.getElementById("tasks");
+let due = document.getElementById("due");
+let desc = document.getElementById("desc");
+let priority = document.getElementById("priority");
+
+const submit_btn = document.getElementById("submit");
+const projects = document.getElementById("projects");
+
 // Constructor Function for Task objects
 function Task(proj,tasks,desc,due,priority) {
-    this.proj_title = proj;
-    this.tasks_list = tasks;
-    this.dueDate = due;
-    this.description = desc;
-    this.priority = priority;
+    this.proj_title = proj.value;
+    this.tasks_list = tasks.value;
+    this.dueDate = due.value;
+    this.description = desc.value;
+    this.priority = priority.value;
   }
 
-// Create a Person object
-const task1 = new Task("do dishes",["wash plate","wash fork"],"make sink clean",2/5/2025,1);
+// creates Task from form data
+function createTask() {
+    const task = new Task(proj,tasks,desc,due,priority);
+    task_list.push(task)
 
-// Display age
-console.log("My project is " + task1.proj_title + " and it involves " + task1.tasks_list);
-console.log(task1);
+    displayList();
+}
+
+function displayList() {
+    projects.textContent = ""; // Clear previous tasks
+
+    task_list.forEach((task, index) => {
+        const taskItem = document.createElement("div");
+        taskItem.textContent = `
+        Project ${index + 1}: ${task.proj_title} | 
+        Task: ${task.tasks_list} | 
+        Description: ${task.description} | 
+        Due: ${task.dueDate} | 
+        Priority: ${task.priority}`
+        ;
+        projects.appendChild(taskItem);
+    });
+}
+
+submit_btn.addEventListener("click", createTask); 
+submit_btn.addEventListener("click", displayList); 
